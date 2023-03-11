239b61c99515176e84da9a8565ed03f67a8a865a
Andrés Giacosa <36309452+Panthersoul@users.noreply.github.com>
2023-03-11 17:54:31 -0300

    agrego rutas dinamicas

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ import { Router } from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+
+//const __dirname = dirname(fileURLToPath(import.meta.url));
 const router = Router();
 
 
